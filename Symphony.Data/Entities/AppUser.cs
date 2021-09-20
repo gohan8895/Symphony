cd8f794c9b1f7a73b28ee3c86acff38ab359590f
patrickha55 <pattrickha@gmail.com>
2021-09-20 13:00:43 +0700

    phat

--- Symphony.Data/Entities/AppUser.cs
+++ Symphony.Data/Entities/AppUser.cs
@@ -9,23 +9,14 @@ namespace Symphony.Data.Entities
 {
     public class AppUser : IdentityUser<Guid>
     {
-        [PersonalData]
         public string FirstName { get; set; }
-        [PersonalData]
         public string LastName { get; set; }
-        [PersonalData]
         public string Address { get; set; }
-        [PersonalData]
         public DateTime DOB { get; set; }
-        [PersonalData]
         public char Gender { get; set; }
-        [PersonalData]
         public int BatchId { get; set; }
-        [PersonalData]
         public DateTime CreatedAt { get; set; }
-        [PersonalData]
         public DateTime UpdatedAt { get; set; }
-        [PersonalData]
         public Batch Batch { get; set; }
         public List<Student_Course> Student_Courses { get; set; }
         public List<Student_In_Exam> Student_In_Exams { get; set; }
