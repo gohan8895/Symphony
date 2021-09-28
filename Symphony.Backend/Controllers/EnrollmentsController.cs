cb0db9eaa61fa5c1209d42d3a489a70463fc3659
Tuyet nhung <thanhtong6063@gmail.com>
2021-09-28 20:43:32 +0700

    add more feature enrollment

--- Symphony.Backend/Controllers/EnrollmentsController.cs
+++ Symphony.Backend/Controllers/EnrollmentsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Symphony.Services.BackendServices.EnrollmentServices;
+using Symphony.ViewModels.Consult;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,36 +14,88 @@ namespace Symphony.Backend.Controllers
     [ApiController]
     public class EnrollmentsController : ControllerBase
     {
+        private readonly IEnrollmentService enrollmentService;
+
+        public EnrollmentsController(IEnrollmentService enrollmentService)
+        {
+            this.enrollmentService = enrollmentService;
+        }
+
+
         // GET: api/<EnrollmentsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<EnrollmentVM>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(await enrollmentService.GetEnrollmentVMsAsync());
         }
 
         // GET api/<EnrollmentsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<EnrollmentVM>> Get(int id)
         {
-            return "value";
+            var enrollment = await enrollmentService.GetEnrollmentVMAsync(id);
+            if(enrollment is null)
+            {
+                return NotFound();
+            }
+            return Ok(enrollment);
         }
 
         // POST api/<EnrollmentsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<EnrollmentVM>> Post([FromBody] CreateEnrollmentVM createEnrollmentVM)
         {
+            if(createEnrollmentVM is null)
+            {
+                return BadRequest();
+            }
+            var enrollmentVM = new EnrollmentVM();
+            if(ModelState.IsValid)
+            {
+                enrollmentVM = await enrollmentService.CreateEnrollment(createEnrollmentVM);
+            }
+
+            return CreatedAtAction(nameof(Get), new { id = enrollmentVM.Id }, enrollmentVM);
         }
 
         // PUT api/<EnrollmentsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult<EnrollmentVM>> Put([FromBody] UpdateEnrollmentVM updateEnrollmentVM)
         {
+            if(updateEnrollmentVM is null)
+            {
+                return BadRequest();
+            }
+            var enrollmentVM = new EnrollmentVM();
+            if(ModelState.IsValid)
+            {
+                enrollmentVM = await enrollmentService.UpdateEnrollment(updateEnrollmentVM);
+            }
+                
+            if(enrollmentVM is null) 
+            { 
+                return NotFound(); 
+            }
+            return NoContent();
         }
 
-        // DELETE api/<EnrollmentsController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // UPDATE Enrollemnt Status
+        [HttpPut("update-enrollment-state/{id}")]
+        public async Task<ActionResult> ChangeStatus(int id)
         {
+            var enrollment = await enrollmentService.GetEnrollmentVMAsync(id);
+            if(enrollment is null)
+            {
+                return NotFound();
+            }
+            await enrollmentService.ChangeEnrollmentStatus(id);
+            return NoContent();
+        }
+
+        [HttpGet("enrollment/get-enrollment-with-data")]
+        public async Task<ActionResult<EnrollmentWithData>> GetEnrollmentWithData()
+        {
+            return Ok(await enrollmentService.GetEnrollmentWithDataVM());
         }
     }
 }
--- Symphony.Services/BackendServices/EnrollmentServices/EnrollmentService.cs
+++ Symphony.Services/BackendServices/EnrollmentServices/EnrollmentService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Symphony.Data.EF;
+using Symphony.Data.Entities;
+using Symphony.ViewModels.Consult;
+using Symphony.ViewModels.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,84 @@ using System.Threading.Tasks;
 
 namespace Symphony.Services.BackendServices.EnrollmentServices
 {
-    class EnrollmentService
+    public class EnrollmentService : IEnrollmentService
     {
+        private readonly SymphonyDBContext symphonyDBContext;
+
+        public EnrollmentService(SymphonyDBContext symphonyDBContext)
+        {
+            this.symphonyDBContext = symphonyDBContext;
+        }
+
+        public async Task<EnrollmentVM> GetEnrollmentVMAsync(int id)
+        {
+            var enrollment = await symphonyDBContext.Enrollments.FirstOrDefaultAsync(e => e.Id == id);
+            if (enrollment is null)
+            {
+                return null;
+            }
+            return enrollment.EnsVM();
+        }
+        public async Task<IEnumerable<EnrollmentVM>> GetEnrollmentVMsAsync()
+        {
+            var enrollments = await symphonyDBContext.Enrollments.Select(e => e.EnsVM()).ToListAsync();
+            return enrollments;
+        }
+
+        public async Task<EnrollmentVM> CreateEnrollment(CreateEnrollmentVM enrollmentVM)
+        {
+            var enrollment = new Enrollment()
+            {
+                UserId = enrollmentVM.UserId,
+                CourseId = enrollmentVM.CourseId,
+                IsDelete = enrollmentVM.IsDelete
+            };
+
+            await symphonyDBContext.Enrollments.AddAsync(enrollment);
+            await symphonyDBContext.SaveChangesAsync();
+
+            return enrollment.EnsVM();
+        }
+        public async Task<EnrollmentVM> UpdateEnrollment(UpdateEnrollmentVM enrollmentVM)
+        {
+            var enrollment = await symphonyDBContext.Enrollments.FirstOrDefaultAsync(e => e.Id == enrollmentVM.Id);
+            if(enrollment is null)
+            {
+                return null;
+            }
+            
+            enrollment.UserId = enrollmentVM.UserId;
+            enrollment.CourseId = enrollmentVM.CourseId;
+            enrollment.IsDelete = enrollmentVM.IsDelete;
+
+            await symphonyDBContext.SaveChangesAsync();
+            return enrollment.EnsVM();
+        }
+        public async Task ChangeEnrollmentStatus(int id)
+        {
+            var enrollment = await symphonyDBContext.Enrollments.FirstOrDefaultAsync(e => e.Id == id);
+            var status = enrollment.IsDelete;
+            if (enrollment.IsDelete == false)
+            {
+                enrollment.IsDelete = true;
+            }
+            else enrollment.IsDelete = false;
+            await symphonyDBContext.SaveChangesAsync();
+        }
+        public async Task<IEnumerable<EnrollmentWithData>> GetEnrollmentWithDataVM()
+        {
+            var enrollments = await symphonyDBContext.Enrollments.Select(e => new EnrollmentWithData()
+            {
+                Id = e.Id,
+                UserId = e.UserId,
+                CourseId = e.CourseId,
+                IsDelete = e.IsDelete,
+                AppUserVM = e.AppUser.AsVM(),
+                CourseVM = e.Course.AsVM()
+            }).ToListAsync();
+
+            return enrollments;
+            
+        }
     }
 }
