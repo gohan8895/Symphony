9e4dba181397c5fdd20d9eacfba06ece1a89a938
hoangthach1402 <vuhoangthach1402@gmail.com>
2021-09-30 10:06:26 +0700

    .

---
+++ Symphony.BlazorServerApp/Services/FaqServices/IFaqService.cs
@@ -0,0 +1,17 @@
+﻿using Symphony.ViewModels.Consult;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Symphony.BlazorServerApp.Services.FaqServices
+{
+    public interface IFaqService
+    {
+        Task<IEnumerable<FAQVM>> GetFaqsAsync();
+        Task<FAQVM> GetFaqAsync(int id);
+        Task CreateFaqAsync(CreateFAQVM faq);
+        Task UpdateFaqAsync(UpdateFAQVM faq);
+        Task DeleteFaqAsync(int id);
+    }
+}
