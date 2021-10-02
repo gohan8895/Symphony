f13ad5357a6da4685397107185b5b1c22afd5d63
PatrickHa <diepvien004b@gmail.com>
2021-10-02 17:39:19 +0700

    phat

---
+++ Symphony.BlazorServerApp/Services/SubjectServices/ISubjectService.cs
@@ -0,0 +1,21 @@
+﻿using Symphony.ViewModels.Consult;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Symphony.BlazorServerApp.Services.SubjectServices
+{
+    public interface ISubjectService
+    {
+        Task<SubjectVM> GetSubjectVMAsync(int id);
+
+        Task<IEnumerable<SubjectVM>> GetSubjectVMsAsync();
+
+        Task CreateSubjectVMAsync(SubjectCreateRequest createRequest);
+
+        Task UpdateSubjectVMAsync(SubjectUpdateRequest subjectVM);
+
+        Task ChangeSubjectState(int id);
+    }
+}
---
+++ Symphony.BlazorServerApp/Services/SubjectServices/SubjectService.cs
@@ -0,0 +1,47 @@
+﻿using Symphony.ViewModels.Consult;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Symphony.BlazorServerApp.Services.SubjectServices
+{
+    public class SubjectService : ISubjectService
+    {
+        private readonly IHttpClientFactory clientFactory;
+        private readonly JsonSerializerOptions options;
+
+        public SubjectService(IHttpClientFactory clientFactory)
+        {
+            this.clientFactory = clientFactory;
+            options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public Task<IEnumerable<SubjectVM>> GetSubjectVMsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SubjectVM> GetSubjectVMAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateSubjectVMAsync(SubjectCreateRequest createRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateSubjectVMAsync(SubjectUpdateRequest subjectVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangeSubjectState(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
