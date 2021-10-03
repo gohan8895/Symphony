0fb311764765506bcb1af0df2cbf4cfdf354c00f
hoangthach1402 <vuhoangthach1402@gmail.com>
2021-10-03 17:39:53 +0700

    event

---
+++ Symphony.BlazorServerApp/Services/EventServices/EventService.cs
@@ -0,0 +1,101 @@
+﻿using Symphony.ViewModels.Consult;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Symphony.BlazorServerApp.Services.EventServices
+{
+    public class EventService : IEventService
+    {
+        private readonly IHttpClientFactory clientFactory;
+        private readonly JsonSerializerOptions options;
+        public EventService(IHttpClientFactory clientFactory)
+        {
+            this.clientFactory = clientFactory;
+            options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public async Task CreateEventAsync(CreateEventVM item)
+        {
+
+            if (item is not null)
+            {
+                var client = clientFactory.CreateClient("symphony");
+                var faqJson = new StringContent(
+                    JsonSerializer.Serialize(item, options),
+                    encoding: Encoding.UTF8,
+                    "application/json"
+                    );
+
+                using var httpResponse = await client.PostAsync("events/", faqJson);
+
+                httpResponse.EnsureSuccessStatusCode();
+            }
+        }
+
+        public async Task DeleteEventAsync(int id)
+        {
+            if (id != 0)
+            {
+                var client = clientFactory.CreateClient("symphony");
+                using var httpResponse = await client.DeleteAsync("events/" + $"{id}");
+
+                httpResponse.EnsureSuccessStatusCode();
+            }
+        }
+
+        public async Task<EventVM> GetEventAsync(int id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"events/{id}");
+            var client = clientFactory.CreateClient("symphony");
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var reponseStream = await response.Content.ReadAsStreamAsync();
+                return await JsonSerializer.DeserializeAsync<EventVM>(reponseStream, options);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<IEnumerable<EventVM>> GetEventsAsync()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "events/");
+            var client = clientFactory.CreateClient("symphony");
+            var response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                using var reponseStream = await response.Content.ReadAsStreamAsync();
+                return await JsonSerializer.DeserializeAsync<IEnumerable<EventVM>>(reponseStream, options);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task UpdateEventAsync(UpdateEventVM item)
+        {
+            if (item is not null)
+            {
+                var client = clientFactory.CreateClient("symphony");
+                var faqJson = new StringContent(
+                    JsonSerializer.Serialize(item, options),
+                    encoding: Encoding.UTF8,
+                    "application/json"
+                    );
+
+                using var httpResponse = await client.PutAsync("events/", faqJson);
+
+                httpResponse.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
---
+++ Symphony.BlazorServerApp/Services/EventServices/IEventService.cs
@@ -0,0 +1,17 @@
+﻿using Symphony.ViewModels.Consult;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Symphony.BlazorServerApp.Services.EventServices
+{
+   public interface IEventService
+    {
+        Task<IEnumerable<EventVM>> GetEventsAsync();
+        Task<EventVM> GetEventAsync(int id);
+        Task CreateEventAsync(CreateEventVM item);
+        Task UpdateEventAsync(UpdateEventVM item);
+        Task DeleteEventAsync(int id);
+    }
+}
