bc9f1b5e0afc46751694b9f1209b86be1de080aa
Trung Nguyen <trung.ntt.ueh@outlook.com>
2021-10-03 09:09:10 +0700

    add exam_result

---
+++ Symphony.ViewModels/Extensions/Exam_ResultExtension.cs
@@ -0,0 +1,26 @@
+﻿using Symphony.Data.Entities;
+using Symphony.ViewModels.AboutViewModel;
+using Symphony.ViewModels.Exam_ResultViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symphony.ViewModels.Extensions
+{
+    public static class Exam_ResultExtension
+    {
+        public static Exam_ResultVM AsVM(this Exam_Result result) => new Exam_ResultVM
+        {
+            Id = result.Id,
+            ExamId = result.ExamId,
+            ExamName = result.Exam.Name,
+            UserId = result.UserId,
+            StudentName = result.AppUser.FirstName + " " + result.AppUser.LastName,
+            CreatedAt = result.CreatedAt,
+            TotalScore = result.TotalScore,
+            Result = result.HasPassed ? "Passed" : "Failed"
+        };
+    }
+}
